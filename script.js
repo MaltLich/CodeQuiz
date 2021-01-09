7b9d09b025905c2b729d0eb5bab388583f2ccfd1
Andrew Novak <andrew.n.novak@gmail.com>
2021-01-08 21:11:44 -0500

    Updated style.css, created script.js, filled script.js with objects and arrays for questions

---
+++ script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Commonly used date types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+
+];
